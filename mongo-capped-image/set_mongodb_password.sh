d4b8368019fe05af2f2719f3aa3fa7b40fa43f9d
Angel Barrera <angelbarrerasanchez@gmail.com>
2017-04-30 17:37:55 +0200

    added mongo straming collection

---
+++ mongo-capped-image/set_mongodb_password.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+USER=${MONGODB_USER:-"admin"}
+DATABASE=${MONGODB_DATABASE:-"admin"}
+PASS=${MONGODB_PASS:-"admin123"}
+CAPPEDCOLLECTION=${MONGOCAPPEDCOLLECTION:-"no"}
+_word=$( [ ${MONGODB_PASS} ] && echo "preset" || echo "random" )
+
+RET=1
+while [[ RET -ne 0 ]]; do
+    echo "=> Waiting for confirmation of MongoDB service startup"
+    sleep 5
+    mongo admin --eval "help" >/dev/null 2>&1
+    RET=$?
+done
+
+echo "=> Creating an ${USER} user with a ${_word} password in MongoDB"
+mongo admin --eval "db.createUser({user: '$USER', pwd: '$PASS', roles:[{role:'root',db:'admin'}]});"
+
+if [ "$DATABASE" != "admin" ]; then
+    echo "=> Creating an ${USER} user with a ${_word} password in MongoDB"
+    mongo admin -u $USER -p $PASS << EOF
+use $DATABASE
+db.createUser({user: '$USER', pwd: '$PASS', roles:[{role:'dbOwner',db:'$DATABASE'}]})
+EOF
+fi
+
+if [ "$CAPPEDCOLLECTION" == "yes" ]; then
+	echo "=> Creating a capped collection with a size of 5242880 bytes or 5000 documents"
+	mongo admin -u $USER -p $PASS << EOF
+use $DATABASE
+db.createCollection("streaming", { capped : true, size : 5242880, max : 5000 } )
+EOF
+fi
+
+echo "=> Done!"
+touch /data/db/.mongodb_password_set
+
+echo "========================================================================"
+echo "You can now connect to this MongoDB server using:"
+echo ""
+echo "    mongo $DATABASE -u $USER -p $PASS --host <host> --port <port>"
+echo ""
+echo "Please remember to change the above password as soon as possible!"
+echo "========================================================================"
