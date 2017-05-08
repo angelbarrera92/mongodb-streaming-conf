5f059db057a019a79f49597f262a2680bcf8cb20
Angel Barrera <angelbarrerasanchez@gmail.com>
2017-05-08 22:52:12 +0200

    first running version

--- python-server/app.py
+++ python-server/app.py
@@ -1,51 +1,37 @@
 from flask import Flask, render_template
-from flask_socketio import emit, SocketIO
-from pymongo import MongoClient, CursorType
-# Flask
+from flask_socketio import SocketIO
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-# Socket.io
-socketio = SocketIO(app)
+socket_io = SocketIO(app)
+thread = None
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', async_mode=socket_io.async_mode)
 
 
-@socketio.on('connected')
+@socket_io.on('connected')
 def handle_json(json):
     print('received json: ' + str(json))
-    from threading import Thread
-    thread1 = Thread(target=emit_db_events)
-    thread1.start()
+    global thread
+    if thread is None:
+        thread = socket_io.start_background_task(target=mongo_streams)
 
 
-def get_cursor(collection):
-    print 'getting cursor'
-    return collection.find({}, cursor_type=CursorType.TAILABLE_AWAIT)
-
-
-def emit_db_events():
-    # Mongo
-    mongo_connection = MongoClient(host='127.0.0.1', port=27017)
-    db = mongo_connection.get_database('admin')
+def mongo_streams():
+    from pymongo import MongoClient, CursorType
+    connection = MongoClient(host='127.0.0.1', port=27017)
+    db = connection.get_database('admin')
     db.authenticate(name='admin', password='admin123')
     collection = db.get_collection('streaming')
-    from bson.json_util import dumps
-    print 'starting!!!'
-    import time
     while True:
-        cur = get_cursor(collection)
-        while cur.alive:
-            for message in cur:
-                print(message)
-                socketio.emit('log', {'hi': 'mama'}, callback=received())
-            time.sleep(0.1)
-
-
-def received():
-    print('received in front')
+        cursor = collection.find({}, cursor_type=CursorType.TAILABLE_AWAIT)
+        while cursor.alive:
+            for message in cursor:
+                message.pop('_id', None)
+                socket_io.emit('log', message)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socket_io.run(app)
